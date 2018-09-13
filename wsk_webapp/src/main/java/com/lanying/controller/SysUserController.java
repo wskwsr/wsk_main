65ecaf3e620e89244e1e84cd626bcafa9e949894
weishaokui <wskemail>
2018-09-13 21:04:30 +0800

    wskcnm2222

--- wsk_webapp/src/main/java/com/lanying/controller/SysUserController.java
+++ wsk_webapp/src/main/java/com/lanying/controller/SysUserController.java
@@ -21,6 +21,7 @@ public class SysUserController {
 	@RequestMapping("queryList.do")
 	public List<SysUser> queryList() {
 		//wskcnm
+        System.out.print("wskcnm");
 		return sysUserService.queryList();
 	}
 }
